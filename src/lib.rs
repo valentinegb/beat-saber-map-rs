e3e4caac2ee16efd26a563ccb4c00f6534178ac1
Valentine Briese <valentinegb@icloud.com>
2024-08-01 20:31:15 -0700

    Add `BeatSaberMap::from_dir`

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,10 @@
 mod hex;
 pub mod info;
 
+use std::{fs, path::PathBuf};
+
+use serde::de::Error;
+
 pub use self::info::Info;
 
 /// Structural representation of a Beat Saber map folder.
@@ -18,4 +22,34 @@ pub struct BeatSaberMap {
     ///
     /// See [`Info`].
     pub info: Info,
+}
+
+impl BeatSaberMap {
+    /// Deserializes the files in a map folder.
+    pub fn from_dir(dir: impl Into<PathBuf>) -> serde_json::Result<Self> {
+        Ok(BeatSaberMap {
+            info: serde_json::from_str(&fs::read_to_string(dir.into().join("Info.dat")).map_err(
+                |err| {
+                    let err_string = err.to_string();
+
+                    serde_json::Error::custom(match err_string.chars().nth(0) {
+                        Some(first_char) => {
+                            first_char.to_lowercase().to_string() + &err_string[1..]
+                        }
+                        None => err_string,
+                    })
+                },
+            )?)?,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_dir_doesnt_fail() {
+        BeatSaberMap::from_dir("sample").unwrap();
+    }
 }
